929625e3db6aafaed66a27538ee06520e8c2c7ba
Mackwell <mackwellm@gmail.com>
2025-10-21 14:56:21 +0200

    Second Assignment First Test

---
+++ RomanNumerals/RomNum.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class RomNum
+{
+    public int Results()
+    {
+        return 0;
+    }
+
+    public void Conv(int i)
+    {
+        //
+    }
+}
+
