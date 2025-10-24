12c6754a1b837da0fa5e25dc5eb629ffef4e26bf
Mackwell <mackwellm@gmail.com>
2025-10-24 10:05:30 +0200

    First Test - out of range

--- RomanNumerals/RomNum.cs
+++ RomanNumerals/RomNum.cs
@@ -6,18 +6,8 @@ public class RomNum
 
     public int num;
     public string Results()
-    {   
-        
-        if (num < 1 || num < 3000)
-        {
-            string result = "Not in range";
-            return result;
-        }
-        else
-        {
-            string result = "In range";
-            return result;
-        }
+    {
+        return "";
     }
 
     public string InRang(int num)
